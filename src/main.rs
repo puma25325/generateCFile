d22d9a75f3cc5a238b71b0fc0e1b51c9665d076d
puma25325 <fassiongear@gmail.com>
2017-08-13 12:00:00 -0600

    fix:main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+extern "C" {
+    fn say_hello(name: *const i8);
+}
 fn main() {
-    println!("Hello, world!");
+    let name = "Anthony";//std::ffi::CString::new("World").unwrap();
+    unsafe {
+        say_hello(name.as_ptr());
+    }
 }
