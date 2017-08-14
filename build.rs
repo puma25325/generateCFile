91e352c58e4db7e8e5dcc1d8b072afbf9e74aab1
puma25325 <fassiongear@gmail.com>
2017-08-14 12:00:00 -0600

    fix:c func parameter type

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 
 fn main(){
     let src = Path::new("example.c");
-    let out_dir = env::var("OUT_DIR").unwrap();
+    env::var("OUT_DIR").unwrap();
     cc::Build::new()
         .file(src)
         .compile("example");
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-extern "C" {
-    fn say_hello(name: *const i8);
+unsafe extern "C" {
+    unsafe fn say_hello(name: *const i8);
 }
 fn main() {
-    let name = "Anthony";//std::ffi::CString::new("World").unwrap();
+    let name = std::ffi::CString::new("World").unwrap();
     unsafe {
         say_hello(name.as_ptr());
     }
