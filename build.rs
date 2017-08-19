75c68273739dcdfd80c029081c486531fdabec6f
puma25325 <fassiongear@gmail.com>
2017-08-19 12:00:00 -0600

    fix:build.rs-->make binding code

--- build.rs
+++ build.rs
@@ -1,11 +1,29 @@
 use std::env;
-use std::path::Path;
+use std::path::PathBuf;
 
 fn main(){
-    let src = Path::new("example.c");
-    env::var("OUT_DIR").unwrap();
+    let src_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
+        .join("bindC")
+        .join("src");
+    let include_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
+        .join("bindC")
+        .join("include");
     cc::Build::new()
-        .file(src)
+        .file(src_dir.join("example.c"))
+        .include(&include_dir)
         .compile("example");
-    print!("cargo:rerun-if-changed={}", src.display());
+    let header_path = include_dir.join("example.h");
+    let include_dir_str = include_dir.to_str().unwrap().to_owned();
+    let bindings = bindgen::Builder::default()
+        .header(header_path.to_str().unwrap())
+        .clang_arg("-I")
+        .clang_arg(&include_dir_str)
+        .generate()
+        .expect("Unable to generate bindings");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+    println!("cargo:rerun-if-changed=bindC/src/example.c");
+    println!("cargo:rerun-if-changed=bindC/include/example.h");
 }
